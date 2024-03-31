bb06a82de9bc41dd937e2cd3db815ecfd78a9bc9
SSalmanAli <syedsalmanali228s@gmail.com>
2024-03-31 18:23:55 +0500

    now it can also delete added items
    now it can also delete added items

--- index.js
+++ index.js
@@ -2,48 +2,60 @@
 // let todos = [];
 // let condition = true;
 // let i = 0;
-// while(condition){
-// let addTask = await inquirer.prompt(
-//     [
-//     {
-//         name:"todo",
-//         type:"input",
-//         message:"What do you want to add to your To-Do list ?"
-//     },
-//     {
-//         name:"addMore" ,
-//         type:"confirm" ,
-//         message: "Do you want to add more ?",
-//         default:"false"
-//     }
-// ]
-// );
-// todos.push((addTask.todo));
+// while (condition) {
+//     let addTask = await inquirer.prompt([
+//         {
+//             name: "todo",
+//             type: "input",
+//             message: "What do you want to add to your To-Do list ?"
+//         },
+//         {
+//             name: "addMore",
+//             type: "confirm",
+//             message: "Do you want to add more ?",
+//             default: false
+//         }
+//     ]);
+//     todos.push(addTask.todo);
 //     i++;
-// todos.forEach((y , index)=> console.log(index + i, y));
-// condition = addTask.addMore
+//     console.log("Your To-Do List:");
+//     todos.forEach((task, index) => console.log(`${index + 1}. ${task}`));
+//     condition = addTask.addMore;
 // }
 import inquirer from "inquirer";
 let todos = [];
 let condition = true;
-let i = 0;
 while (condition) {
     let addTask = await inquirer.prompt([
         {
             name: "todo",
             type: "input",
-            message: "What do you want to add to your To-Do list ?"
+            message: "What do you want to add to your To-Do list?"
         },
         {
             name: "addMore",
             type: "confirm",
-            message: "Do you want to add more ?",
+            message: "Do you want to add more?",
             default: false
         }
     ]);
     todos.push(addTask.todo);
-    i++;
     console.log("Your To-Do List:");
     todos.forEach((task, index) => console.log(`${index + 1}. ${task}`));
+    let deleteTask = await inquirer.prompt({
+        name: "deleteIndex",
+        type: "number",
+        message: "Enter the number of the task you want to delete (0 to cancel):",
+        validate: (input) => {
+            if (input < 0 || input > todos.length) {
+                return "Please enter a valid task number.";
+            }
+            return true;
+        }
+    });
+    if (deleteTask.deleteIndex !== 0) {
+        todos.splice(deleteTask.deleteIndex - 1, 1);
+        console.log("Task deleted successfully.");
+    }
     condition = addTask.addMore;
 }
